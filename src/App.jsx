5cb1c0a28ae663ba03d5981e304273609c92dbb0
ahmed7gendy <106438956+ahmed7gendy@users.noreply.github.com>
2024-04-22 14:37:30 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import myImg from './src/assets/my.jpg'
+
+function App() {
+  const personalInfo = {
+    name: 'Ahmed Tarek Gendy',
+    title: 'Web Developer',
+    email: 'ahmed7gendy@gmail.com',
+    phone: '+201156265436',
+    address: 'Modern Academy Street, Mokattam, Cairo, Egypt',
+  }
+
+  const skills = ['JavaScript', 'React', 'HTML', 'CSS']
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="profile-container">
+          <img src={myImg} alt="Ahmed Tarek Gendy" className="my-img" />
+          <div className="personal-info">
+            <h1>{personalInfo.name}</h1>
+            <h2>{personalInfo.title}</h2>
+          </div>
+        </div>
+        
+        <div className="skills">
+          <h3>Skills</h3>
+          <ul>
+            {skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
